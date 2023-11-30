6b26eb41304a9d61cbc71164c0e07b794d95de7c
lalitnailwal <36082917+lalitnailwal@users.noreply.github.com>
2023-11-30 21:07:34 +0530

    Adding top loading bar v-36

--- src/components/News.js
+++ src/components/News.js
@@ -35,15 +35,19 @@ export class News extends Component {
     }
 
     async updateNews() {
+        this.props.setProgress(10);
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bf737f27c0854a498dfa529f8dbb9da7&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
+        this.props.setProgress(30);
         let parsedData = await data.json() 
+        this.props.setProgress(70);
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false
         })
+        this.props.setProgress(100);
     }
 
     async componentDidMount(){
