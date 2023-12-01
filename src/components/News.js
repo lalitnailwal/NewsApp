42fa6a4eaed478dba67230c97f262a60056ec10f
lalitnailwal <36082917+lalitnailwal@users.noreply.github.com>
2023-12-01 11:53:06 +0530

    Sticky Navbar and Bug Fixes V-40

--- src/components/News.js
+++ src/components/News.js
@@ -9,8 +9,7 @@ const News = (props) => {
     const[articles, setArticles] = useState([])
     const[loading, setLoading] = useState(true)
     const[page, setPage] = useState(1)
-    const[totalResults, setTotalResults] = useState(0)
-    //document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
+    const[totalResults, setTotalResults] = useState(0)    
 
     const capitalizeFirstLetter = (string)=> {
         return string.charAt(0).toUpperCase() + string.slice(1);
@@ -31,22 +30,13 @@ const News = (props) => {
     }
 
     useEffect(() => {
+        document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
         updateNews(); 
     }, []);
 
-    const handlePrevClick = async ()=>{
-        setPage(page - 1);
-        updateNews();
-    }
-
-    const handleNextClick = async ()=>{
-        setPage(page + 1);
-        updateNews()
-    }
-
     const fetchMoreData = async () => {  
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
         setPage(page + 1);    
-        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json()
         setArticles(articles.concat(parsedData.articles));
@@ -55,7 +45,7 @@ const News = (props) => {
 
     return (
      <>
-        <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
+        <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
         {loading && <Spinner/>}
         <InfiniteScroll
                     dataLength={articles.length}
